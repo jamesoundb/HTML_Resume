90a8d7a7a38680d250c0c8d4c6ba5311fe61010f
James Wurbel <jameswurbel@gmail.com>
2023-04-18 09:03:14 -0700

    Update apifetch_jameswurbel.js

--- apifetch_jameswurbel.js
+++ apifetch_jameswurbel.js
@@ -1,4 +1,4 @@
 const visitorCount = process.env.VISITOR_COUNT;
-fetch('visitorCount')
+fetch(visitorCount)
   .then(response => response.json())
   .then(data => document.getElementById('N').innerText=data)
