a5dfb8115dbca80d712ef702e5d39208e11dad11
James Wurbel <jameswurbel@gmail.com>
2023-04-18 10:14:37 -0700

    Update apifetch_jameswurbel.js

--- apifetch_jameswurbel.js
+++ apifetch_jameswurbel.js
@@ -1,4 +1,6 @@
 const visitorEndpoint = process.env.VISITOR_ENDPOINT;
+console.log("<<<<<<<Visitor Endpoint>>>>>>")
+console.log(visitorEndpoint)
 fetch(visitorEndpoint)  
   .then(response => response.json())
   .then(data => document.getElementById('N').innerText=data)
